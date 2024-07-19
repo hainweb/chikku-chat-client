78bc2c89bd7db2e1dd23d639d4b2fda4e6348d6b
hainweb <hainofficialweb@gmail.com>
2024-07-19 21:43:46 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,13 +42,7 @@ const Home = () => {
   // added 
  
 
-socket.on('connect', () => {
-  console.log('Connected to WebSocket server');
-});
 
-socket.on('disconnect', () => {
-  console.log('Disconnected from WebSocket server');
-});
 
   /***socket connection */
   useEffect(()=>{
