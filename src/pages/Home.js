1ad376e7d9f8629be89ad50a7af5cf264d838326
hainweb <hainofficialweb@gmail.com>
2024-07-20 08:44:20 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,98 +1,104 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice'
-import Sidebar from '../components/Sidebar'
-import logo from '../assets/logo.png'
-import io from 'socket.io-client'
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice';
+import Sidebar from '../components/Sidebar';
+import logo from '../assets/logo.png';
+import io from 'socket.io-client';
 
 const Home = () => {
-  const user = useSelector(state => state.user)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const location = useLocation()
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  console.log('user',user)
-  const fetchUserDetails = async()=>{
+  console.log('user', user);
+
+  const fetchUserDetails = async () => {
     try {
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios({
-          url : URL,
-          withCredentials : true
-        })
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
+      console.log('Fetching user details from URL:', URL); // Add logging
+      const response = await axios({
+        url: URL,
+        withCredentials: true
+      });
 
-        dispatch(setUser(response.data.data))
+      dispatch(setUser(response.data.data));
 
-        if(response.data.data.logout){
-            dispatch(logout())
-            navigate("/email")
-        }
-        console.log("current user Details",response)
+      if (response.data.data.logout) {
+        dispatch(logout());
+        navigate("/email");
+      }
+      console.log("current user Details", response);
     } catch (error) {
-        console.log("error",error)
+      console.log("error", error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    fetchUserDetails()
-  },[])
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
 
-
-  // added 
-
-
-
-
+  // Added socket connection
 
   /***socket connection */
-  useEffect(()=>{
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
-      auth : {
-        token : localStorage.getItem('token')
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    console.log('Attempting to connect to socket with token:', token); // Add logging
+    const socketConnection = io(process.env.REACT_APP_BACKEND_URL, {
+      auth: {
+        token: token
       },
-       transports: ['websocket', 'polling'],
-  withCredentials: true
-    })
+      transports: ['websocket', 'polling'],
+      withCredentials: true
+    });
 
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
-      dispatch(setOnlineUser(data))
-    })
+    socketConnection.on('onlineUser', (data) => {
+      console.log('Online users:', data); // Add logging
+      dispatch(setOnlineUser(data));
+    });
 
-    dispatch(setSocketConnection(socketConnection))
+    socketConnection.on('connect', () => {
+      console.log('Socket connected:', socketConnection.id); // Add logging
+    });
 
-    return ()=>{
-      socketConnection.disconnect()
-    }
-  },[])
+    socketConnection.on('connect_error', (error) => {
+      console.error('Socket connection error:', error); // Add logging
+    });
 
+    dispatch(setSocketConnection(socketConnection));
 
-  const basePath = location.pathname === '/'
+    return () => {
+      socketConnection.disconnect();
+    };
+  }, [dispatch]);
+
+  const basePath = location.pathname === '/';
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
-           <Sidebar/>
-        </section>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <Sidebar />
+      </section>
 
-        {/**message component**/}
-        <section className={`${basePath && "hidden"}`} >
-            <Outlet/>
-        </section>
+      {/**message component**/}
+      <section className={`${basePath && "hidden"}`}>
+        <Outlet />
+      </section>
 
-
-        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div>
-              <img
-                src={logo}
-                width={250}
-                alt='logo'
-              />
-            </div>
-            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+        <div>
+          <img
+            src={logo}
+            width={250}
+            alt='logo'
+          />
         </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
